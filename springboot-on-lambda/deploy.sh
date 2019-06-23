b4bf4a9a5cec34c30c9e2125ced72e6e40aaf8b3
kimkao <yikaikao@gmail.com>
2019-06-23 15:59:30 +0800

    update path for bootstrap

--- springboot-on-lambda/deploy.sh
+++ springboot-on-lambda/deploy.sh
@@ -11,7 +11,7 @@ function bundle() {
     mvn clean package -DskipTests=true -Dnative=true -Dnative-image.docker-build=true
 
     mkdir -p ${BUNDLEDIR}
-    cp -r target/wiring-classes/bootstrap ${APPDIR}/*-runner  ${BUNDLEDIR}
+    cp -r bootstrap ${APPDIR}/*-runner  ${BUNDLEDIR}
     chmod 755 ${BUNDLEDIR}/bootstrap
     cd ${BUNDLEDIR} && zip -q function.zip bootstrap function.sh *-runner ; cd -
 
