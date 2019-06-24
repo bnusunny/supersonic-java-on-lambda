95568c99fc237438633c2aafa367afa04eabee39
humank <yikaikao@gmail.com>
2019-06-24 08:29:04 +0000

    correct up bootstrap file.

--- springboot-on-lambda/deploy.sh
+++ springboot-on-lambda/deploy.sh
@@ -7,20 +7,16 @@ BUNDLEDIR=target/bundle
 
 function bundle() {
     rm -f response.txt
-    rm hello.zip
-    mvn clean package
+    rm hello.zip hello
+    mvn clean test package
 
-    
-    native-image \
-        --verbose \
+    native-image --no-fallback \
+        --report-unsupported-elements-at-runtime \
         -jar target/springboot-on-lambda-0.0.1.jar \
-        -H:Name=hello \
-        -H:+ReportUnsupportedElementsAtRuntime \
-        -H:-AllowVMInspection \
-        -R:-InstallSegfaultHandler
+        -H:Name=hello
 
-    chmod 755 bootstrap hello function.sh
-    zip -q hello.zip bootstrap hello function.sh
+    chmod 755 bootstrap hello
+    zip hello.zip bootstrap hello
 
     echo deploy hello.zip to AWS Lambda
 
@@ -39,7 +35,7 @@ aws lambda create-function \
     --function-name nativesb \
     --timeout 10 \
     --zip-file fileb://hello.zip \
-    --handler function.sh \
+    --handler hello \
     --runtime provided \
     --role ${LAMBDA_ROLE_ARN}
 
